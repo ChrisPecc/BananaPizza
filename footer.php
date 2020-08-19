3a51f865ab201ad57bc4e2c45ea3fa36ac3ca11b
Michael <marino.michael.1990@gmail.com>
2020-08-19 12:46:41 +0200

    end footer

--- footer.php
+++ footer.php
@@ -1,115 +1,121 @@
 
 <footer>
     <img class="footer-hachures" src="<?php bloginfo('template_url'); ?>/assets/images/hachures-grises.png" alt="">
-    <div class="footer-info">
-        <div class="footer-info-text">
-            <h2>BananaPizza (mettre php)</h2>
-            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. At voluptate vel, fuga id eos illo ipsa aliquid esse sit quae quod quis! Sint quod commodi delectus odio, quaerat quisquam provident.</p>
-        </div>
-        <div class="footer-info-social">
-            <a href="#" class="footer-fb"><img src="http://localhost:8888/wordpress/wp-content/themes/BananaPizza/assets/svg/facebook.svg" alt=""></a>
-            <a href="#" class="footer-twitter"><img src="<?php bloginfo('template_url'); ?>/assets/svg/twitter.svg" alt=""></a>
-            <a href="#" class="footer-insta"><img src="<?php bloginfo('template_url'); ?>/assets/svg/instagram.svg" alt=""></a>
-            <a href="#" class="footer-linkedin"><img src="<?php bloginfo('template_url'); ?>/assets/svg/linkedin.svg" alt=""></a>
-        </div>
-    </div>
-    <div class="footer-contact-hour">
-        <div class="footer-open">
-        <h3>Open Hours</h3>
-            <div class="footer-days-open">
-                <div class="footer-hour">
-                    <img src="<?php bloginfo('template_url'); ?>/assets/svg/time-clock.svg" alt="">
-                </div>
-                <div class="footer-days-hour">
-                        <p>Mondays</p>
-                        <p>----</p>
-                        <p>php here</p>
-                </div>
+    <div class="footer-container">
+    
+        <div class="footer-info">
+            <h2>mettre php</h2>
+            <div class="footer-info-text">
+                
+                <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. At voluptate vel, fuga id eos illo ipsa aliquid esse sit quae quod quis!</p>
             </div>
-            <div class="footer-days-open">
-                <div class="footer-hour">
-                    <img src="<?php bloginfo('template_url'); ?>/assets/svg/time-clock.svg" alt="">
-                </div>
-                <div class="footer-days-hour">
-                        <p>Tue-Fri</p>
-                        <p>----</p>
-                        <p>php here</p>
-                </div>
-            </div>
-            <div class="footer-days-open">
-                <div class="footer-hour">
-                    <img src="<?php bloginfo('template_url'); ?>/assets/svg/time-clock.svg" alt="">
-                </div>
-                <div class="footer-days-hour">
-                        <p>Sat-Sun</p>
-                        <p>----</p>
-                        <p>php here</p>
-                </div>
-            </div>
-            <div class="footer-days-open">
-                <div class="footer-hour">
-                    <img src="<?php bloginfo('template_url'); ?>/assets/svg/time-clock.svg" alt="">
-                </div>
-                <div class="footer-days-hour">
-                        <p>Public Holidays</p>
-                        <p>----</p>
-                        <p>php here</p>
-                </div>
+            <div class="footer-info-social">
+                <a href="#" class="footer-fb"><img src="<?php bloginfo('template_url'); ?>/assets/svg/facebook.svg" alt=""></a>
+                <a href="#" class="footer-twitter"><img src="<?php bloginfo('template_url'); ?>/assets/svg/twitter.svg" alt=""></a>
+                <a href="#" class="footer-insta"><img src="<?php bloginfo('template_url'); ?>/assets/svg/instagram.svg" alt=""></a>
+                <a href="#" class="footer-linkedin"><img src="<?php bloginfo('template_url'); ?>/assets/svg/linkedin.svg" alt=""></a>
             </div>
         </div>
-        <div class="footer-contact">
-            <h3>Contact Us</h3>
-            <div class="footer-contact-line">
-                <div class="footer-logo-contact">
-                    <p>ici icone tel</p>
+        <div class="footer-contact-hour">
+            <div class="footer-open">
+                <h3>Open Hours</h3>
+                <div class="footer-days-open">
+                    <div class="footer-hour">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/svg/time-clock.svg" alt="">
+                    </div>
+                    <div class="footer-days-hour">
+                            <p>Mondays</p>
+                            <p>----</p>
+                            <p>php here</p>
+                    </div>
                 </div>
-                <div class="footer-info-contact">
-                    <p>ici num telephone php</p>
+                <div class="footer-days-open">
+                    <div class="footer-hour">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/svg/time-clock.svg" alt="">
+                    </div>
+                    <div class="footer-days-hour">
+                            <p>Tue-Fri</p>
+                            <p>----</p>
+                            <p>php here</p>
+                    </div>
+                </div>
+                <div class="footer-days-open">
+                    <div class="footer-hour">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/svg/time-clock.svg" alt="">
+                    </div>
+                    <div class="footer-days-hour">
+                            <p>Sat-Sun</p>
+                            <p>----</p>
+                            <p>php here</p>
+                    </div>
+                </div>
+                <div class="footer-days-open">
+                    <div class="footer-hour">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/svg/time-clock.svg" alt="">
+                    </div>
+                    <div class="footer-days-hour">
+                            <p>Public Holidays</p>
+                            <p>----</p>
+                            <p>php here</p>
+                    </div>
                 </div>
             </div>
-            <div class="footer-contact-line">
-                <div class="footer-logo-contact">
-                    <p>ici icone lieu</p>
+            <div class="footer-contact">
+                <h3>Contact Us</h3>
+                <div class="footer-contact-line">
+                    <div class="footer-logo-contact">
+                        <img src="<?php bloginfo('template_url'); ?>/assets/svg/call.svg" alt="">
+                    </div>
+                    <div class="footer-info-contact">
+                        <p>ici num telephone php</p>
+                    </div>
                 </div>
-                <div class="footer-info-contact">
-                    <p>ici num adresse php</p>
-                    <p>ici num code postal php</p>
-                    <p>ici pays php</p>
+                <div class="footer-contact-line">
+                    <div class="footer-logo-contact">
+                    <img src="<?php bloginfo('template_url'); ?>/assets/svg/place.svg" alt="">
+                    </div>
+                    <div class="footer-info-contact">
+                        <p>ici num adresse php</p>
+                        <p>ici num code postal php</p>
+                        <p>ici pays php</p>
+                    </div>
+                </div>
+                <div class="footer-contact-line">
+                    <div class="footer-logo-contact">
+                    <img src="<?php bloginfo('template_url'); ?>/assets/svg/mail-1.svg" alt="">
+                    </div>
+                    <div class="footer-info-contact">
+                        <p>ici mail php</p>
+                    </div>
                 </div>
             </div>
-            <div class="footer-contact-line">
-                <div class="footer-logo-contact">
-                    <p>ici icone mail</p>
-                </div>
-                <div class="footer-info-contact">
-                    <p>ici mail php</p>
-                </div>
+        </div>
+        <div class="footer-insta">
+            <h3>Instagram</h3>
+            <div class="footer-imgbox">
+                <img src="<?php bloginfo('template_url'); ?>/assets/images/plat1.jpg" alt="">
+            </div>
+            <div class="footer-imgbox">
+                <img src="<?php bloginfo('template_url'); ?>/assets/images/plat2.jpg" alt="">
+            </div>
+            <div class="footer-imgbox">
+                <img src="<?php bloginfo('template_url'); ?>/assets/images/plat3.jpg" alt="">
+            </div>
+            <div class="footer-imgbox">
+                <img src="<?php bloginfo('template_url'); ?>/assets/images/plat4.jpg" alt="">   
+            </div>
+            <div class="footer-imgbox">
+                <img src="<?php bloginfo('template_url'); ?>/assets/images/plat5.jpg" alt="">
+            </div>
+            <div class="footer-imgbox">
+                <img src="<?php bloginfo('template_url'); ?>/assets/images/plat6.jpg" alt="">   
             </div>
         </div>
-    </div>
-    <div class="footer-insta">
-        <div class="footer-imgbox">
-            <img src="" alt="">
+        <div class="footer-copyright">
+            <hr>
+            <p>© 2020 All Rights Reserved. Designed By BananaPizza Studio</p>
         </div>
-        <div class="footer-imgbox">
-            <img src="" alt="">
-        </div>
-        <div class="footer-imgbox">
-            <img src="" alt="">
-        </div>
-        <div class="footer-imgbox">
-            <img src="" alt="">
-        </div>
-        <div class="footer-imgbox">
-            <img src="" alt="">
-        </div>
-        <div class="footer-imgbox">
-            <img src="" alt="">
-        </div>
-    </div>
-    <div class="footer-copyright">
-        <hr>
-        <p>copyright ta mere</p>
+
     </div>
 
 </footer>
