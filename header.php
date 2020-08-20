e6fe3b089a60af617a3aeade8cd2c46a278408e5
MmarinoM <marino.michael.1990@gmail.com>
2020-08-20 18:01:45 +0200

    start nav

--- header.php
+++ header.php
@@ -4,14 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php bloginfo('name')?> - <?php bloginfo('description');?></title>
-    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="<?php echo get_stylesheet_uri();?>">
 </head>
 <body>
     <header>
         <div class="nav-menu">
             <div class="nav-title">
-            <h1><?php echo bloginfo( 'title' ) ;?></h1>
+            <a href="index.php"><h1><?php echo bloginfo('title') ;?></h1></a>
             </div>
             <div class="nav-ul">
                 <?php wp_nav_menu(); ?>
